882eb981b135b558ecc4a9a32277ab8cb3115642
David-mwendwa <klatzdavie19@gmail.com>
2022-01-24 08:36:43 +0300

    es6: sets

--- es6/index.js
+++ es6/index.js
@@ -8,3 +8,29 @@ const double = (...nums) => {
 const people = ['shaun', 'ryu', 'crystal'];
 const members = ['mario', 'chun', ...people];
 //console.log(members)
+
+// sets - store any kind of unique values
+const letters = ['a', 'b', 'a', 'c', 'd', 'c', 'c'];
+console.log(letters);
+
+const lettersSet = new Set(letters);
+//console.log(lettersSet)
+
+//const uniqueLetters = [...lettersSet]
+const uniqueLetters = [...new Set(letters)];
+console.log(uniqueLetters);
+
+const ages = new Set();
+ages.add(20).add(34).add(67);
+ages.delete(20);
+console.log(ages, ages.size);
+console.log(ages.has(34));
+
+const ninjas = new Set([
+  { name: 'kim', age: 18 },
+  { name: 'aggie', age: 13 },
+  { name: 'john', age: 24 },
+]);
+ninjas.forEach(ninja => {
+  console.log(ninja.name, ninja.age)
+})
