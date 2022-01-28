d1b1a77c9eafd825aa9732914917b9d54f92ecec
David-mwendwa <klatzdavie19@gmail.com>
2022-01-28 12:50:12 +0300

    use babal CLI

---
+++ babel_webpack/after.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// run - node_modules/.bin/babel before.js -o after.js
+var greet = function greet(name) {
+  console.log("Hello ".concat(name));
+};
+
+greet();
---
+++ babel_webpack/before.js
@@ -0,0 +1,6 @@
+// run - node_modules/.bin/babel before.js -o after.js
+
+const greet = name => {
+  console.log(`Hello ${name}`)
+}
+greet()
